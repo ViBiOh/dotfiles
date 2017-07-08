2c8f06703158d21b4454a75b7c018d40b200e2f0
Vincent Boutour <bob@vibioh.fr>
2017-07-08 11:23:44 +0000

    Fixing initial run of install script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 set -e
 
 for file in ${HOME}/code/src/github.com/ViBiOh/dotfiles/symlinks/*; do
-  [ -r "${file}" ] && [ -f "${file}" ] && rm ${HOME}/.`basename ${file}` && ln -s ${file} ${HOME}/.`basename ${file}`
+  [ -r "${file}" ] && [ -f "${file}" ] && rm -f ${HOME}/.`basename ${file}` && ln -s ${file} ${HOME}/.`basename ${file}`
 done
