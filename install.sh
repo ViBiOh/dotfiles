8f600a27b295678caa3fe4927643f1d3d0464cbf
Vincent Boutour <vboutour@meilleursagents.com>
2017-07-11 19:02:38 +0200

    Starting to add snippets

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ set -e
 for file in ${HOME}/code/src/github.com/ViBiOh/dotfiles/symlinks/*; do
   [ -r "${file}" ] && [ -f "${file}" ] && rm -f ${HOME}/.`basename ${file}` && ln -s ${file} ${HOME}/.`basename ${file}`
 done
+
+rm -rf ${HOME}/.vim/snippets
+ln -s ${HOME}/code/src/github.com/ViBiOh/dotfiles/snippets ${HOME}/.vim/snippets
