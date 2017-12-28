360c9beff0bb92a1f156185b54f51a4e01a65734
Vincent Boutour <bob@vibioh.fr>
2017-12-28 08:43:58 +0100

    Ignoring error

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+set +e
+
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 PKG_USER="/Users/`whoami`/Library/Application Support/Sublime Text 3/Packages/User/"
 mkdir -p "${PKG_USER}"
 cp snippets/* "${PKG_USER}"
 cp settings/* "${PKG_USER}"
+
+echo Success!
