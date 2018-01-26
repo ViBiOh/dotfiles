f5901e1ec9e8bd19cb46e423612ab83fdf08c372
Vincent Boutour <bob@vibioh.fr>
2018-01-26 08:58:17 +0100

    Updating installation script

--- install.sh
+++ install.sh
@@ -6,24 +6,43 @@ for file in ${HOME}/code/src/github.com/ViBiOh/dotfiles/symlinks/*; do
   [ -r "${file}" ] && [ -f "${file}" ] && rm -f ${HOME}/.`basename ${file}` && ln -s ${file} ${HOME}/.`basename ${file}`
 done
 
-echo 'Host *
-    AddKeysToAgent yes
+MAX_OS_SSH_CONFIG=""
+if [ `uname` == 'Darwin' ]; then
+  MAX_OS_SSH_CONFIG="
+    UseKeyChain no"
+fi
+
+echo "Host *
+    PasswordAuthentication no
+    ChallengeResponseAuthentication no
+    HashKnownHosts yes${MAX_OS_SSH_CONFIG}
     ServerAliveInterval 300
     ServerAliveCountMax 2
 
-Host example
-    HostName example.domain
+Host vibioh
+    HostName vibioh.fr
     User vibioh
-' >> ${HOME}/.ssh/config
+" >> ${HOME}/.ssh/config
 
 
-if [ `uname` == 'Darwin' ] && [ command -v brew > /dev/null 2>&1 ]; then
+if [ `uname` == 'Darwin' ] && ! command -v brew > /dev/null 2>&1; then
   echo Installing brew
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
   echo
   echo Installing common brew tools
-  brew install bash bash-completion tldr fzf
+  brew install \
+    bash \
+    bash-completion \
+    tldr \
+    fzf \
+    openssl \
+    gnupg
+
+  echo
+  echo Installing curl with right option
+  brew install curl --with-openssl
+  brew link --force curl
 
   echo
   echo Configuring FZF
