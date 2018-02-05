bd97d35c55dbfae0c08df8cc63c000400738f7a2
Vincent Boutour <bob@vibioh.fr>
2018-02-05 23:00:26 +0100

    Adding graphviz for golang profiling

--- install.sh
+++ install.sh
@@ -42,7 +42,8 @@ if [ `uname` == 'Darwin' ] && ! command -v brew > /dev/null 2>&1; then
     tmux \
     reattach-to-user-namespace \
     pass \
-    golang
+    golang \
+    graphviz
 
   echo
   echo Installing curl with right option
