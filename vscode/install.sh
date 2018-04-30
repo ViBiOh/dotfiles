a1d8d0c0a1a3b632201012b4b776a06952ecc96f
Vincent Boutour <bob@vibioh.fr>
2018-04-30 11:56:42 +0200

    Removing git history plugin

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,7 +3,6 @@
 code --install-extension alefragnani.project-manager
 code --install-extension casualjim.gotemplate
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension donjayamanne.githistory
 code --install-extension dzannotti.vscode-babel-coloring
 code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
