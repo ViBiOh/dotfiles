d854f0889a68c768bdcc641d94ea809e94d2dac4
Vincent Boutour <bob@vibioh.fr>
2018-06-19 21:56:54 +0200

    Updating fzf install for disabled prompt

--- install/fzf.sh
+++ install/fzf.sh
@@ -2,21 +2,16 @@
 
 echo -e "${GREEN}FZF${RESET}"
 
-FZF_INSTALL=`brew --prefix`/opt/fzf/install
-
 if [ `uname` == 'Darwin' ]; then
   brew reinstall fzf fd
+  FZF_INSTALL=`brew --prefix`/opt/fzf/install
 else
   if [ ! -d "${HOME}/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
-    FZF_INSTALL="${HOME}/.fzf/install"
   else
-    echo Updating FZF
-
-    cd ${HOME}/.fzf && git pull && ./install
+    cd ${HOME}/.fzf && git pull
   fi
+  FZF_INSTALL="${HOME}/.fzf/install"
 fi
 
-if [ ! -e "${HOME}/.fzf.bash" ]; then
-  FZF_INSTALL --key-bindings --completion --no-update-rc
-fi
+${FZF_INSTALL} --key-bindings --completion --no-update-rc
