29714cbc5a14fb28949e1e5be90cf9218e70c384
Vincent Boutour <bob@vibioh.fr>
2018-06-20 14:03:01 +0200

    Updating fzf config

--- install/fzf.sh
+++ install/fzf.sh
@@ -2,16 +2,10 @@
 
 echo -e "${GREEN}FZF${RESET}"
 
-if [ `uname` == 'Darwin' ]; then
-  brew reinstall fzf fd
-  FZF_INSTALL=`brew --prefix`/opt/fzf/install
+if [ ! -d "${HOME}/.fzf" ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
 else
-  if [ ! -d "${HOME}/.fzf" ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
-  else
-    cd ${HOME}/.fzf && git pull
-  fi
-  FZF_INSTALL="${HOME}/.fzf/install"
+  cd "${HOME}/.fzf" && git pull
 fi
 
-${FZF_INSTALL} --key-bindings --completion --no-update-rc
+"${HOME}/.fzf/install" --key-bindings --completion --no-update-rc
