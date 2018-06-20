5330af2d8218f7f0b29fc32b82680181c417e0e0
Vincent <ViBiOh@users.noreply.github.com>
2018-06-20 15:42:53 +0200

    Delete vim.sh

--- install/vim.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "${GREEN}Vim${RESET}"
-
-if [ ! -e "${HOME}/.vim/autoload/plug.vim" ]; then
-  curl -fLo "${HOME}/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-  vim +PlugInstall +qall
-fi
