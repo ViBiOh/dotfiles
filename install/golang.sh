f9ee26ee767486bbf24f1b77d9cfc031ede9c662
Vincent Boutour <bob@vibioh.fr>
2018-07-27 17:58:30 +0200

    Adding delve

--- install/golang.sh
+++ install/golang.sh
@@ -15,6 +15,7 @@ if command -v go > /dev/null 2>&1; then
   rm -rf ${GOPATH}/bin/* ${GOPATH}/pkg/* ${GOPATH}/src/golang.org
   ls $GOPATH/src/github.com | grep -v ViBiOh | xargs rm -rf
 
+  go get -u github.com/derekparker/delve/cmd/dlv
   go get -u github.com/golang/dep/cmd/dep
   go get -u github.com/golang/lint/golint
   go get -u github.com/google/pprof
