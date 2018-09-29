a4b9be5a0a75f6e0e27ec3fbfc339129501aecc0
Vincent Boutour <bob@vibioh.fr>
2018-09-29 19:10:10 +0200

    Installing ripgrep only on desktop

--- install/ripgrep.sh
+++ install/ripgrep.sh
@@ -10,6 +10,6 @@ echo "-----------"
 if [ `uname -s` == 'Darwin' ]; then
   brew tap burntsushi/ripgrep https://github.com/BurntSushi/ripgrep.git
   brew install ripgrep-bin
-elif command -v apt-get > /dev/null 2>&1; then
+elif command -v apt-get > /dev/null 2>&1 && [ `uname -m` == 'x86_64' ]; then
   sudo apt-get install ripgrep
 fi
