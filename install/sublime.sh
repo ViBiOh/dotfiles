de8d253fba223ac9a44cb02f715e9f785b85fe61
Vincent <ViBiOh@users.noreply.github.com>
2018-10-01 08:57:23 +0200

    Update sublime.sh

--- install/sublime.sh
+++ install/sublime.sh
@@ -11,5 +11,5 @@ if [ `uname` == 'Darwin' ]; then
   brew cask install \
     sublime-text
 
-  ln -f -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /Users/macbook/homebrew/bin
+  ln -f -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "${HOME}/homebrew/bin"
 fi
