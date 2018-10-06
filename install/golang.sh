c2141a7e749988646839c0621e571a41d3348c8c
Vincent Boutour <bob@vibioh.fr>
2018-10-06 14:21:30 +0200

    More smart erase of go binary

--- install/golang.sh
+++ install/golang.sh
@@ -14,7 +14,8 @@ fi
 if command -v go > /dev/null 2>&1; then
   source "${HOME}/code/src/github.com/ViBiOh/dotfiles/sources/golang"
 
-  rm -rf ${GOPATH}/bin/* ${GOPATH}/pkg/* ${GOPATH}/src/golang.org
+  rm -rf ${GOPATH}/pkg/* ${GOPATH}/src/golang.org
+  ls "${GOPATH}/bin" | grep -v "diff-so-fancy" | xargs rm -rf
   ls "${GOPATH}/src/github.com" | grep -v ViBiOh | xargs rm -rf
 
   if [ `uname -m` == 'x86_64' ]; then
