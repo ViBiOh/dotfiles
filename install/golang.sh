48328977443d94aa56143b68b64ac1dad81b594d
Vincent Boutour <bob@vibioh.fr>
2018-10-12 19:15:39 +0200

    fixing url for golint

--- install/golang.sh
+++ install/golang.sh
@@ -22,8 +22,8 @@ if command -v go > /dev/null 2>&1; then
     go get -u github.com/derekparker/delve/cmd/dlv
   fi
   go get -u github.com/golang/dep/cmd/dep
-  go get -u github.com/golang/lint/golint
   go get -u github.com/google/pprof
   go get -u github.com/kisielk/errcheck
+  go get -u golang.org/x/lint/golint
   go get -u golang.org/x/tools/cmd/goimports
 fi
