e247101d5ce761516f8aa8fa179650d59917af95
Vincent Boutour <bob@vibioh.fr>
2018-11-06 13:17:20 +0100

    removing brew node in favor of n

--- install/node.sh
+++ install/node.sh
@@ -4,14 +4,23 @@ set -e
 set -u
 
 echo "--------"
-echo "- Node -"
+echo "- node -"
 echo "--------"
 
 rm -rf "${HOME}/.npm" "${HOME}/.npm_packages"
 
-if [ `uname` == 'Darwin' ]; then
-  brew install node
+if ! command -v make > /dev/null 2>&1; then
+  exit
 fi
+
+git clone --depth 1 https://github.com/tj/n.git "${HOME}/n"
+cd "${HOME}/n"
+sudo PREFIX=/usr/local make install
+cd "${HOME}"
+rm -rf "${HOME}/n"
+
+source "${HOME}/code/src/github.com/ViBiOh/dotfiles/sources/n"
+n latest
 
 if command -v npm > /dev/null 2>&1; then
   npm install --ignore-scripts -g npm npm-check-updates
