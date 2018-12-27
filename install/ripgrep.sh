785fce8d4771a31e16dbec77a610a0b899ae86c2
Vincent Boutour <bob@vibioh.fr>
2018-12-27 22:45:16 +0100

    Adding condition for ripgrep

--- install/ripgrep.sh
+++ install/ripgrep.sh
@@ -7,9 +7,22 @@ echo "-----------"
 echo "- ripgrep -"
 echo "-----------"
 
+RIPGREP_VERSION=0.10.0
+
 if [ "${IS_MACOS}" == true ]; then
   brew tap burntsushi/ripgrep https://github.com/BurntSushi/ripgrep.git
   brew install ripgrep-bin
 elif command -v apt-get > /dev/null 2>&1 && [ `uname -m` == 'x86_64' ]; then
-  sudo apt-get install -y -qq ripgrep
+  set +e
+  sudo apt-cache show ripgrep > /dev/null 2>&1
+  ripgrep=$?
+  set -e
+
+  if [ "${ripgrep}" -eq 0 ]; then
+    sudo apt-get install -y -qq ripgrep
+  else
+    curl -LO "https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}_amd64.deb"
+    sudo dpkg -i "ripgrep_${RIPGREP_VERSION}_amd64.deb"
+    rm -rf "ripgrep_${RIPGREP_VERSION}_amd64.deb"
+  fi
 fi
