b2942fd1b49aa4a59f98f2347bc0820fdba47486
Vincent Boutour <bob@vibioh.fr>
2019-01-28 08:42:34 +0100

    Improving install of sublime texte

--- sublime/install.sh
+++ sublime/install.sh
@@ -8,17 +8,16 @@ readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 main() {
   echo Configuring SublimeText
 
-  GO_PLUGIN="SublimeGo"
-
-  PKG="${HOME}/Library/Application Support/Sublime Text 3/Packages"
-  PKG_USER="${PKG}/User"
+  local GO_PLUGIN="SublimeGo"
+  local PKG="${HOME}/Library/Application Support/Sublime Text 3/Packages"
+  local PKG_USER="${PKG}/User"
 
   mkdir -p "${PKG_USER}"
   rm -rf "${PKG_USER}"/* "${PKG}/${GO_PLUGIN}"
 
-  cp -r "${GO_PLUGIN}" "${PKG}/${GO_PLUGIN}"
-  cp snippets/* "${PKG_USER}/"
-  cp settings/* "${PKG_USER}/"
+  cp -r "${SCRIPT_DIR}/${GO_PLUGIN}" "${PKG}/${GO_PLUGIN}"
+  cp "${SCRIPT_DIR}/snippets/"* "${PKG_USER}/"
+  cp "${SCRIPT_DIR}/settings/"* "${PKG_USER}/"
 
   if command -v go > /dev/null 2>&1; then
     go get -u github.com/mdempsky/gocode
