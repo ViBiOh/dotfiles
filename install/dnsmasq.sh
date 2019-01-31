74121c5aa2a1173307dcfd514323a5a0dda98fce
Vincent Boutour <bob@vibioh.fr>
2019-01-31 21:04:28 +0100

    Updating DNS configuration

--- install/dnsmasq.sh
+++ install/dnsmasq.sh
@@ -5,6 +5,14 @@ set -o nounset
 set -o pipefail
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+sed_inplace() {
+  if [[ "${IS_MACOS}" == true ]]; then
+    sed -i '' "${@}"
+  else
+    sed -i "${@}"
+  fi
+}
+
 main() {
   if command -v pihole > /dev/null 2>&1; then
     echo "pihole found, no action"
@@ -12,19 +20,25 @@ main() {
   fi
 
   local DNSMASQ_CONF='/etc/dnsmasq.conf'
+  local DNSCRYPT-PROXY_CONF='/etc/dnscrypt-proxy'
 
   if [[ "${IS_MACOS}" == true ]]; then
-    brew install dnsmasq --with-dnssec
+    brew install dnsmasq dnscrypt-proxy --with-dnssec
     DNSMASQ_CONF=$(brew --prefix)/etc/dnsmasq.conf
   elif command -v apt-get > /dev/null 2>&1; then
     sudo apt-get install -y -qq dnsmasq
   fi
 
-  echo '# Forward queries to DNSCrypt on localhost port 5355
-server=127.0.0.1#5355
+  if [[ -e "$(brew --prefix)/etc/dnscrypt-proxy.toml" ]]; then
+    sed_inplace "s|^listen_addresses.*$|listen_addresses = ['127.0.0.1:5355']|g" /Users/macbook/homebrew/etc/dnscrypt-proxy.toml
+  fi
 
-# Uncomment to forward queries to Google Public DNS, if DNSCrypt is not used
-# You may also use your own DNS server or other public DNS server you trust
+  echo "listen-address=127.0.0.1
+listen-address=$(hostname -I)
+bind-interfaces
+
+# Forward queries to DNSCrypt on localhost port 5355
+server=127.0.0.1#5355
 server=1.1.1.1
 server=1.0.0.1
 
@@ -36,6 +50,7 @@ bogus-priv
 
 # Reject private addresses from upstream nameservers
 stop-dns-rebind
+rebind-localhost-ok
 
 # Query servers in order
 strict-order
@@ -47,15 +62,16 @@ cache-size=8192
 # Optional logging directives
 log-async
 log-dhcp
-log-facility=/var/log/dnsmasq.log' | sudo tee "${DNSMASQ_CONF}" > /dev/null
+log-facility=/var/log/dnsmasq.log" | sudo tee "${DNSMASQ_CONF}" > /dev/null
 
   if [[ "${IS_MACOS}" == true ]]; then
+    sudo brew services restart dnscrypt-proxy
     sudo brew services restart dnsmasq
     sudo networksetup -setdnsservers "Wi-Fi" 127.0.0.1
     scutil --dns
     networksetup -getdnsservers "Wi-Fi"
   elif command -v apt-get > /dev/null 2>&1; then
-    sudo /etc/init.d/dnsmasq restart
+    sudo systemctl restart dnsmasq
   fi
 }
 
