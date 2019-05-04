2e685b2e9c679ef13f93591ee6ec565f6aa3aed8
Vincent Boutour <bob@vibioh.fr>
2019-05-04 11:45:32 +0200

    Forcing update of brew cask dependencies

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -4,7 +4,7 @@ set -o nounset -o pipefail -o errexit
 
 main() {
   if command -v brew > /dev/null 2>&1; then
-    brew cask install alacritty
+    brew cask reinstall alacritty
     ln -f -s /Applications/Alacritty.app/Contents/MacOS/alacritty "${HOME}/opt/bin/alacritty"
   fi
 }
