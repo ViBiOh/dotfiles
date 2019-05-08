acdc32a542e5be28431fd21efecbadc1aec93bf6
Vincent Boutour <bob@vibioh.fr>
2019-05-08 18:55:15 +0200

    Updating cleaning

--- install/_clean.sh
+++ install/_clean.sh
@@ -3,7 +3,7 @@
 set -o nounset -o pipefail -o errexit
 
 main() {
-  rm -rf "${HOME}/opt" "${HOME}/.config"
+  sudo rm -rf "${HOME}/opt" "${HOME}/.config" "${HOME}/.fzf.bash"
   mkdir -p "${HOME}/opt/bin"
 }
 
