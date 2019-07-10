3eb3e7a7c5f678a2b013b0d2f9da832e1cf5795d
Vincent Boutour <bob@vibioh.fr>
2019-07-10 14:47:48 +0200

    Cleaning secret file

--- install/_clean.sh
+++ install/_clean.sh
@@ -3,7 +3,13 @@
 set -o nounset -o pipefail -o errexit
 
 main() {
-  sudo rm -rf "${HOME}/opt" "${HOME}/.config" "${HOME}/.fzf.bash"
+  sudo rm -rf "${HOME}/.ansible-vault-pass"
+  sudo rm -rf "${HOME}/.config"
+  sudo rm -rf "${HOME}/.fzf.bash"
+  sudo rm -rf "${HOME}/.scwrc"
+  sudo rm -rf "${HOME}/.terraformrc"
+  sudo rm -rf "${HOME}/opt"
+
   mkdir -p "${HOME}/opt/bin"
 }
 
