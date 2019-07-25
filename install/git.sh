0c01d6b10fe86bec01df2435901c30bdc2be9043
Vincent Boutour <bob@vibioh.fr>
2019-07-25 09:40:32 +0200

    Adding git prompt into sources folder

--- install/git.sh
+++ install/git.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -o nounset -o pipefail -o errexit
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 main() {
   if [[ "${OSTYPE}" =~ ^darwin ]]; then
@@ -15,12 +16,7 @@ main() {
     return
   fi
 
-  if command -v pacman > /dev/null 2>&1; then
-    local GIT_COMPLETION="git-prompt.sh"
-
-    curl -O  "https://raw.githubusercontent.com/git/git/v$(git --version | awk '{print $3}')/contrib/completion/${GIT_COMPLETION}"
-    sudo mv "${GIT_COMPLETION}" "/usr/share/bash-completion/completions/git"
-  fi
+  curl -o "${SCRIPT_DIR}/../sources/git-prompt" "https://raw.githubusercontent.com/git/git/v$(git --version | awk '{print $3}')/contrib/completion/git-prompt.sh"
 
   if command -v perl > /dev/null 2>&1; then
     curl -o "${HOME}/opt/bin/diff-so-fancy" "https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy"
