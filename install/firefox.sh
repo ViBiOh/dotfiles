87b07c501ef80254675bf17eeef9fc6995ec5398
Vincent Boutour <bob@vibioh.fr>
2019-07-25 22:43:58 +0200

    Removing firefox installation on macos

--- install/firefox.sh
+++ install/firefox.sh
@@ -3,9 +3,7 @@
 set -o nounset -o pipefail -o errexit
 
 main() {
-  if command -v brew > /dev/null 2>&1; then
-    brew cask reinstall firefox
-  elif command -v pacman > /dev/null 2>&1; then
+  if command -v pacman > /dev/null 2>&1; then
     sudo pacman -S --noconfirm --needed firefox
   fi
 }
