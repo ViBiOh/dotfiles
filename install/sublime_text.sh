d0a3a34eea8449f6432a629ab9fea36e53e39484
Vincent Boutour <bob@vibioh.fr>
2019-07-29 08:38:07 +0200

    Updating sublime text installation

--- install/sublime_text.sh
+++ install/sublime_text.sh
@@ -3,8 +3,8 @@
 set -o nounset -o pipefail -o errexit
 
 main() {
-  if command -v brew > /dev/null 2>&1; then
-    brew cask reinstall sublime-text
+  if [[ "${OSTYPE}" =~ ^darwin ]] && [[ -f "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" ]]; then
+    ln -s "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" "${HOME}/opt/bin/subl"
   elif command -v pacman > /dev/null 2>&1; then
     if [[ $(cat "/etc/pacman.conf" | grep "sublime-text" | wc -l) -eq 0 ]]; then
       local SUBLIME_TEXT_SIGN_KEY="8A8F901A"
@@ -15,7 +15,7 @@ main() {
       sudo pacman-key --lsign-key "${SUBLIME_TEXT_SIGN_KEY}"
       rm "${SUBLIME_TEXT_KEY_FILE}"
 
-      echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a "/etc/pacman.conf"
+      echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/dev/x86_64" | sudo tee -a "/etc/pacman.conf"
 
       sudo pacman -Syuq --noconfirm
     fi
