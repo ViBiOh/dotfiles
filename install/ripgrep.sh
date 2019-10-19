5e92b5c0340777bb38a7a1621bf8dad5021aa499
Vincent Boutour <bob@vibioh.fr>
2019-10-19 22:45:48 +0200

    Bumping terraform

--- install/ripgrep.sh
+++ install/ripgrep.sh
@@ -6,7 +6,7 @@ install() {
   if command -v brew > /dev/null 2>&1; then
     brew tap "burntsushi/ripgrep" "https://github.com/BurntSushi/ripgrep.git"
     brew install ripgrep-bin
-  elif command -v apt-get > /dev/null 2>&1 && [[ "$(uname -m | tr "[:upper:]" "[:lower:]")" = "x86_64" ]]; then
+  elif command -v apt-get > /dev/null 2>&1; then
     sudo apt-get install -y -qq ripgrep
   elif command -v pacman > /dev/null 2>&1; then
     sudo pacman -S --noconfirm --needed ripgrep
