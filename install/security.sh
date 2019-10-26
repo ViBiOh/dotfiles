667032d8df02e7aaeb712e10ec142d833cc6c5f3
Vincent Boutour <bob@vibioh.fr>
2019-10-26 18:46:28 +0200

    Changing key repeat time

--- install/security.sh
+++ install/security.sh
@@ -48,8 +48,8 @@ install() {
     defaults write NSGlobalDomain AppleShowAllExtensions -bool true
     defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
     defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
-    defaults write NSGlobalDomain InitialKeyRepeat -int 10
-    defaults write NSGlobalDomain KeyRepeat -int 1
+    defaults write NSGlobalDomain InitialKeyRepeat -int 15
+    defaults write NSGlobalDomain KeyRepeat -int 2
     defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
     defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
     defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
