cf5b6e753f5feeb960b5d871f0496f03e4511f58
Vincent Boutour <bob@vibioh.fr>
2020-01-06 19:40:46 +0100

    feat(sublime): Adding SublimeLayout plugin width default keymap

---
+++ sublime/plugins/SublimeLayout/layout.py
@@ -0,0 +1,26 @@
+import sublime_plugin
+
+
+class SplitVertically(sublime_plugin.TextCommand):
+    def run(self, edit):
+        window = self.view.window()
+
+        window.set_layout({
+            "cols": [0.0, 0.5, 1.0],
+            "rows": [0.0, 1.0],
+            "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
+        })
+        window.run_command("move_to_group", {
+            "group": 1
+        })
+
+
+class ResetLayout(sublime_plugin.TextCommand):
+    def run(self, edit):
+        window = self.view.window()
+
+        window.set_layout({
+            "cols": [0.0, 1.0],
+            "rows": [0.0, 1.0],
+            "cells": [[0, 0, 1, 1]]
+        })
