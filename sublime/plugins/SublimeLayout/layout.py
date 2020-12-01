c86cac85a010e9a3f6f6f0d478132352d5f12170
Vincent Boutour <bob@vibioh.fr>
2020-12-01 21:47:04 +0100

    fix(sublime): Adding focus  prior to move to another group

--- sublime/plugins/SublimeLayout/layout.py
+++ sublime/plugins/SublimeLayout/layout.py
@@ -12,6 +12,7 @@ class SplitVertically(sublime_plugin.TextCommand):
                 "cells": [[0, 0, 1, 1], [1, 0, 2, 1]],
             }
         )
+        window.run_command("focus_group", {"group": 0})
         window.run_command("move_to_group", {"group": 1})
 
 
