4514c7db8523ae59ae079cda301423fed94b1563
Vincent Boutour <bob@vibioh.fr>
2024-05-02 14:28:18 +0200

    feat(github): Adding support for extra params
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- sources/github.sh
+++ sources/github.sh
@@ -4,7 +4,11 @@ github() {
   meta_check "var" "http"
 
   http_init_client --header "Authorization: Bearer $(github_token)" --header "Accept: application/vnd.github+json" --header "X-GitHub-Api-Version: 2022-11-28"
-  http_request "https://api.github.com${1}"
+
+  local GITHUB_API_PATH="${1-}"
+  shift || true
+
+  http_request "https://api.github.com${GITHUB_API_PATH}" "${@}"
 
   if ! [[ ${HTTP_STATUS} =~ 2.. ]]; then
     http_handle_error
