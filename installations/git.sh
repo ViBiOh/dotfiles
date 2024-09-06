4162551c9dd8a33a7239250d3825e67c9c04716d
Vincent Boutour <bob@vibioh.fr>
2024-09-06 15:31:19 +0200

    feat: Cleaning git prompt and completion
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- installations/git.sh
+++ installations/git.sh
@@ -26,6 +26,4 @@ install() {
   fi
 
   curl_to_binary "https://raw.githubusercontent.com/newren/git-filter-repo/main/git-filter-repo" "git-filter-repo"
-
-  curl --disable --silent --show-error --location --max-time 30 --output "$(script_dir)/../sources/git-prompt" "https://raw.githubusercontent.com/git/git/v$(git --version | awk '{printf("%s", $3)}')/contrib/completion/git-prompt.sh"
 }
