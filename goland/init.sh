e5de34854b8593b7c04f47501631ee28bed59256
Vincent Boutour <bob@vibioh.fr>
2024-11-26 23:03:00 +0100

    feat(goland): Adding basic config
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

---
+++ goland/init.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o nounset -o pipefail -o errexit
+
+script_dir() {
+  local FILE_SOURCE="${BASH_SOURCE[0]}"
+
+  if [[ -L ${FILE_SOURCE} ]]; then
+    dirname "$(readlink "${FILE_SOURCE}")"
+  else
+    (
+      cd "$(dirname "${FILE_SOURCE}")" && pwd
+    )
+  fi
+}
+
+main() {
+  local SCRIPT_DIR
+  SCRIPT_DIR="$(script_dir)"
+
+  echo todo
+}
+
+main "${@}"
