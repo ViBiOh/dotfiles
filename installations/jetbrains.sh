5cb6b512df03d3fc9895e3c4f3a5e7514803708f
Vincent Boutour <bob@vibioh.fr>
2024-11-28 20:08:24 +0100

    feat(goland): Revamp install
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- installations/jetbrains.sh
+++ installations/jetbrains.sh
@@ -3,15 +3,13 @@
 set -o nounset -o pipefail -o errexit
 
 clean() {
-  if command -v gpgconf >/dev/null 2>&1; then
-    gpgconf --kill gpg-agent
+  if [[ ${OSTYPE} =~ ^darwin ]]; then
+    rm -rf "${HOME}/Caches/JetBrains"
   fi
 }
 
 install() {
   packages_install "goland"
 
-  if [[ ${OSTYPE} =~ ^darwin ]]; then
-    rm -rf "${HOME}/Library/JetBrains"
-  fi
+  "$(script_dir)/../goland/init.sh"
 }
