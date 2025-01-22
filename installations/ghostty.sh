a89842b11983154d2bf8861fca7fde53233d5db8
Vincent Boutour <bob@vibioh.fr>
2025-01-22 17:44:40 +0100

    feat: Adding ghostty configuration
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

---
+++ installations/ghostty.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o nounset -o pipefail -o errexit
+
+clean() {
+  rm -rf "${HOME}/.config/ghostty/"
+}
+
+install() {
+  if package_exists "ghostty"; then
+    packages_install_desktop "ghostty"
+  fi
+
+  mkdir -p "${HOME}/.config/pgcli"
+
+  echo 'scrollback-limit = 10000000
+
+macos-titlebar-style = "hidden"
+macos-auto-secure-input = true
+
+quit-after-last-window-closed = true
+
+font-size = 13
+font-feature = "-calt"
+' >"${HOME}/.config/ghostty/config"
+}
