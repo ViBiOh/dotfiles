82d10b4c32f250cd539a5888cfc65f8381ffefac
Vincent Boutour <bob@vibioh.fr>
2025-04-20 19:45:22 +0200

    fix(macos): Remove disappeared option
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- installations/_macos.sh
+++ installations/_macos.sh
@@ -185,7 +185,6 @@ install() {
     sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned on
     sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
     sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
-    sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on
     sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
     sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setblockall off
     sudo pkill -HUP socketfilterfw
