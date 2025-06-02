7bc0ead26bd742a6fe379ee2de9aad5f9fb0f1eb
Vincent Boutour <bob@vibioh.fr>
2025-06-02 21:08:22 +0200

    feat: Adding choice on type of permalink
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- sublime/plugins/SublimeGit/open.py
+++ sublime/plugins/SublimeGit/open.py
@@ -11,8 +11,19 @@ from .utils import git_path
 origin_regex = re.compile("^.*@(.*):(.*?)(.git)?\\n?$")
 
 
+class SublimeGitType(sublime_plugin.ListInputHandler):
+    def name(self):
+        return "git_type"
+
+    def list_items(self):
+        return ["Last SHA", "File last SHA", "Branch", "Default branch"]
+
+
 class SublimeGitWeb(sublime_plugin.WindowCommand):
-    def run(self):
+    def input(self, args):
+        return SublimeGitType()
+
+    def run(self, git_type):
         window = self.window
         if not window:
             return
@@ -43,31 +54,64 @@ class SublimeGitWeb(sublime_plugin.WindowCommand):
             print("unable to get remote push url: {}".format(e.output.decode("utf8")))
             return
 
-        try:
-            branch = (
-                subprocess.check_output(
-                    [
-                        "git",
-                        "log",
-                        "-n",
-                        "1",
-                        "--pretty=format:%h",
-                    ],
-                    stderr=subprocess.STDOUT,
-                    cwd=git_info["root"],
+        if git_type in ["Last SHA", "File last SHA"]:
+            args = [
+                "git",
+                "log",
+                "-n",
+                "1",
+                "--pretty=format:%h",
+            ]
+
+            if git_type == "File last SHA":
+                args += ["--", git_info["path"]]
+
+            try:
+                ref = (
+                    subprocess.check_output(
+                        args,
+                        stderr=subprocess.STDOUT,
+                        cwd=git_info["root"],
+                    )
+                    .decode("utf8")
+                    .rstrip()
                 )
-                .decode("utf8")
-                .rstrip()
-            )
-        except subprocess.CalledProcessError as e:
-            print(
-                "unable to get last commit of file: {}".format(e.output.decode("utf8"))
-            )
-            return
+            except subprocess.CalledProcessError as e:
+                print("unable to get SHA: {}".format(e.output.decode("utf8")))
+                return
+
+        if git_type in ["Branch", "Default branch"]:
+            args = [
+                "git",
+                "rev-parse",
+                "--abbrev-ref",
+            ]
+
+            if git_type == "Branch":
+                args += ["HEAD"]
+            elif "Default branch":
+                args += ["origin/HEAD"]
+
+            try:
+                ref = (
+                    subprocess.check_output(
+                        args,
+                        stderr=subprocess.STDOUT,
+                        cwd=git_info["root"],
+                    )
+                    .decode("utf8")
+                    .rstrip()
+                )
+
+                if git_type == "Default branch":
+                    ref = ref.removeprefix("origin/")
+            except subprocess.CalledProcessError as e:
+                print("unable to get branch: {}".format(e.output.decode("utf8")))
+                return
 
         paths = origin_regex.findall(remote)
         url = "https://{}/{}/blob/{}/{}#L{}".format(
-            paths[0][0], paths[0][1], branch, git_info["path"], line_number
+            paths[0][0], paths[0][1], ref, git_info["path"], line_number
         )
 
         sublime.set_clipboard(url)
