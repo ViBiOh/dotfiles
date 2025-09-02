0cbc1814c09a78b384098436fd3050997b3ce4ee
Vincent Boutour <bob@vibioh.fr>
2025-09-02 18:23:53 +0200

    feat: Adding source link
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- sources/dev.sh
+++ sources/dev.sh
@@ -190,9 +190,6 @@ stock() {
   printf -- "\n"
 
   if command -v spark >/dev/null 2>&1; then
-    printf -- "Volume 1d\n"
-    printf -- "%s" "${YAHOO_OUTPUT}" | jq -r '.chart.result[0].indicators.quote[0].volume | join(",")' | spark
-
     YAHOO_OUTPUT="$(
       curl \
         --disable \
@@ -221,6 +218,8 @@ stock() {
     printf -- "\n1mo %b%s%s%b\n" "${OUTPUT_COLOR}" "${EVOLUTION_SIGN}" "${EVOLUTION_PERCENT%00}%" "${RESET}"
     printf -- "%s" "${YAHOO_OUTPUT}" | jq -r '.chart.result[0].indicators.quote[0].open | join(",")' | spark
   fi
+
+  printf "\nSource: https://finance.yahoo.com/quote/%s\n" "${1:-DDOG}"
 }
 
 date_in() {
