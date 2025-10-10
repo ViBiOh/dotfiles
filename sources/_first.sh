c93ccccaec9f73f604d8250ffd4cd8472dad12b4
Vincent Boutour <bob@vibioh.fr>
2025-10-10 17:35:43 +0200

    fix: Handling edge case of path
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- sources/_first.sh
+++ sources/_first.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 add_to_path() {
-  # Remove previous occurence of the directory
+  # Remove previous occurence of the path
   PATH=${PATH/${1}/}
 
-  # Fix possible colon previously here
+  # Fix case when path was at the start
+  PATH=${PATH#:}
+
+  # Fix case when path was in the middle
   PATH=${PATH/::/:}
+
+  # Fix case when path was at the end
+  PATH=${PATH%:}
 
   # Add entry to the beginning of the PATH
   export PATH="${1}:${PATH}"
 }
 
 if [[ -d "${HOME}/opt/bin" ]]; then
-  add_to_path "/opt/homebrew/bin"
   add_to_path "${HOME}/opt/bin"
 fi
