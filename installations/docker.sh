b2a14a1f65759b15709933a17a34b7f52353c90d
Vincent Boutour <bob@vibioh.fr>
2025-12-31 20:09:13 +0100

    feat: Remove symlink for docker
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- installations/docker.sh
+++ installations/docker.sh
@@ -2,10 +2,18 @@
 
 set -o nounset -o pipefail -o errexit
 
+clean() {
+  if [[ -h "${HOME}/opt/bin/docker" ]]; then
+    rm "${HOME}/opt/bin/docker"
+  fi
+}
+
 install() {
   if [[ ${OSTYPE} =~ ^darwin ]]; then
     packages_install "container"
 
+  if ! [[ -h "${HOME}/opt/bin/docker" ]]; then
     ln -s "${BREW_PREFIX}/bin/container" "${HOME}/opt/bin/docker"
+  fi
   fi
 }
