2ae2def68eb2119fe9e4d28b35b06c29c996d919
Vincent Boutour <bob@vibioh.fr>
2025-12-31 15:09:54 +0100

    feat(sublime): Allow parallel runner
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- sublime/plugins/SublimeGo/golangcilint.py
+++ sublime/plugins/SublimeGo/golangcilint.py
@@ -36,4 +36,4 @@ class GolangCILint(Linter):
         if not file.startswith(folder):
             return "true ${file_path}"
 
-        return "golangci-lint run --fix ${file_path}"
+        return "golangci-lint run --fix --allow-parallel-runners ${file_path}"
