00deba35d97f18aa2b2c4c64a64a23c2c5b62aaa
Vincent Boutour <bob@vibioh.fr>
2026-04-11 16:32:37 +0200

    feat: Support json output for ansible vault pass
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- symlinks/.ansible_vault_pass.sh
+++ symlinks/.ansible_vault_pass.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-pass show "dev/ansible"
+OUTPUT_JSON="false"
+
+OPTIND=0
+while getopts ":j" option; do
+  case "${option}" in
+  j)
+    OUTPUT_JSON="true"
+    ;;
+  \?)
+    printf -- "option -%s is invalid\n" "${OPTARG}" 1>&2
+    return 2
+    ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if [[ ${OUTPUT_JSON:-} == "true" ]]; then
+  jq --null-input --compact-output --arg value "$(pass show "dev/ansible")" '{pass: $value}'
+else
+  pass show "dev/ansible"
+fi
