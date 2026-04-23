bd6e0f2c3a58e189e7824458443418fb8e01897f
Vincent Boutour <bob@vibioh.fr>
2026-04-23 22:11:36 +0200

    refactor(git): Changing git-coco to a func to avoid messing with history
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

---
+++ sources/git_coco.sh
@@ -0,0 +1,185 @@
+#!/usr/bin/env bash
+
+git_coco() {
+  _git_coco_guess_component() {
+    local COMPONENTS=()
+
+    add_part() {
+      local WORDS=()
+      read -r -a WORDS <<<"$(printf -- "%s" "${1}" | sed 's|_| |g;s|-| |g;s|\.| |g')"
+
+      COMPONENTS=("${COMPONENTS[@]}" "${WORDS[@]}")
+    }
+
+    for file in $(git diff --name-only --cached); do
+      add_part "$(basename "${file%.*}")"
+
+      local FILE_DIR
+      FILE_DIR="$(dirname "${file}")"
+
+      while [[ ${FILE_DIR} != "." ]]; do
+        add_part "$(basename "${FILE_DIR}")"
+        FILE_DIR="$(dirname "${FILE_DIR}")"
+      done
+    done
+
+    printf -- "%s\n" "${COMPONENTS[@]}" | sort | uniq
+  }
+
+  _git_coco_configure_remote_api() {
+    case $(printf "%s" "${GIT_REPOSITORY}" | jq -r '.url') in
+    "github.com")
+      github_http_init
+      ;;
+    "codeberg.org")
+      codeberg_http_init
+      ;;
+    esac
+  }
+
+  _git_coco_get_remote_api() {
+    case $(printf "%s" "${GIT_REPOSITORY}" | jq -r '.url') in
+    "github.com")
+      printf "https://api.github.com"
+      ;;
+    "codeberg.org")
+      printf "https://codeberg.org"
+      ;;
+    esac
+  }
+
+  _git_coco_get_issue() {
+    _git_coco_configure_remote_api
+
+    http_request "$(_git_coco_get_remote_api)/repos/$(printf "%s" "${GIT_REPOSITORY}" | jq -r '.owner + "/" + .name')/issues?per_page=100"
+
+    if [[ ${HTTP_STATUS} != "200" ]]; then
+      http_handle_error "Unable to list issues"
+      http_reset
+      return
+    fi
+
+    if [[ $(jq --raw-output '[.[] | select(.pull_request | not)] | length' "${HTTP_OUTPUT}") -eq 0 ]]; then
+      rm "${HTTP_OUTPUT}"
+      http_reset
+      return
+    fi
+
+    ISSUES="$(cat <(printf -- "None\n") <(jq --raw-output '.[] | select(.pull_request | not) | "#" + (.number | tostring) + " " + .title' "${HTTP_OUTPUT}") | fzf --height=20 --ansi --reverse --multi --prompt='Closes> ' | awk '{printf("%s", $1)}')"
+    rm "${HTTP_OUTPUT}"
+
+    if [[ ${ISSUES} != "None" ]]; then
+      printf -- "\n\nCloses %s" "${ISSUES}"
+    fi
+
+    http_reset
+  }
+
+  get_co_author() {
+    _git_coco_configure_remote_api
+
+    http_request "$(_git_coco_get_remote_api)/repos/$(printf "%s" "${GIT_REPOSITORY}" | jq -r '.owner + "/" + .name')/contributors?per_page=100"
+
+    if [[ ${HTTP_STATUS} != "200" ]]; then
+      http_handle_error "Unable to list contributors"
+      http_reset
+      return
+    fi
+
+    if [[ $(jq --raw-output 'length' "${HTTP_OUTPUT}") -eq 0 ]]; then
+      http_reset
+      return
+    fi
+
+    local CO_AUTHOR=""
+    CO_AUTHOR="$(cat <(printf -- "None\nOther\n") <(jq --raw-output '.[] | .login' "${HTTP_OUTPUT}") | fzf --height=20 --ansi --reverse --prompt='Co-Author>')"
+    rm "${HTTP_OUTPUT}"
+
+    if [[ ${CO_AUTHOR} == "None" ]]; then
+      http_reset
+      return
+    fi
+
+    if [[ ${CO_AUTHOR} == "Other" ]]; then
+      var_read "CO_AUTHOR"
+    fi
+
+    http_request "$(_git_coco_get_remote_api)/users/${CO_AUTHOR}/events?per_page=100"
+    if [[ ${HTTP_STATUS} != "200" ]]; then
+      http_handle_error "Unable to get user's events"
+      http_reset
+      return
+    fi
+
+    CO_AUTHOR_EMAIL="$(jq --raw-output '.[] | select(.type == "PushEvent") | .payload.commits[] | .author.name + "<" + .author.email + ">"' "${HTTP_OUTPUT}" | sort | uniq | grep -v '\[bot\]' | fzf --height=20 --ansi --reverse --prompt='Email> ')"
+    rm "${HTTP_OUTPUT}"
+
+    if [[ -n ${CO_AUTHOR_EMAIL:-} ]]; then
+      printf -- "\n\nCo-authored-by: %s" "${CO_AUTHOR_EMAIL}"
+    fi
+
+    http_reset
+  }
+
+  if ! git_is_inside; then
+    var_error "not inside a git tree"
+    return 1
+  fi
+
+  git_is_valid_description "value"
+
+  local REMAINING_LENGTH=$((COMMIT_MAX_LENGTH - 1))
+
+  local SCOPE
+  SCOPE="$(for i in "${!CONVENTIONAL_COMMIT_SCOPES[@]}"; do printf -- "%b%s%b %s\n" "${GREEN}" "${i}" "${RESET}" "${CONVENTIONAL_COMMIT_SCOPES[${i}]}"; done | fzf --height=20 --ansi --reverse --prompt "Scope: " | awk '{printf("%s", $1)}')"
+
+  if [[ -z ${SCOPE} ]]; then
+    return 1
+  fi
+
+  printf -- "SCOPE=%s\n" "${SCOPE}"
+  REMAINING_LENGTH=$((REMAINING_LENGTH - ${#SCOPE}))
+
+  local COMPONENT=""
+  COMPONENT="$(cat <(_git_coco_guess_component) <(printf -- "None") | fzf --height=20 --ansi --reverse --prompt='Component:')"
+
+  if [[ ${COMPONENT} == "None" ]]; then
+    COMPONENT=""
+  else
+    printf -v COMPONENT -- "(%s)" "${COMPONENT}"
+  fi
+  printf -- "COMPONENT=%s\n" "${COMPONENT}"
+  REMAINING_LENGTH=$((REMAINING_LENGTH - ${#COMPONENT}))
+
+  local SKIP_CI=""
+  if var_confirm "Skip CI"; then
+    SKIP_CI="[skip ci] "
+    REMAINING_LENGTH=$((REMAINING_LENGTH - ${#SKIP_CI}))
+  fi
+
+  local BREAKING=""
+  if var_confirm "Contains breaking changes"; then
+    BREAKING="!"
+    REMAINING_LENGTH=$((REMAINING_LENGTH - ${#BREAKING}))
+  fi
+
+  local MESSAGE=""
+  printf -- "%d characters remaining\n" "${REMAINING_LENGTH}"
+  var_read MESSAGE
+
+  local ISSUES_STRING=""
+  local CO_AUTHOR_STRING=""
+
+  local GIT_REPOSITORY
+  GIT_REPOSITORY="$(git_repository)"
+
+  if var_confirm "Link issue"; then
+    ISSUES_STRING="$(_git_coco_get_issue)"
+  fi
+
+  if var_confirm "Define co-author"; then
+    CO_AUTHOR_STRING="$(get_co_author)"
+  fi
+
+  var_print_and_run git commit --signoff --message "$(printf -- "%s%s%s: %s%b%b%b" "${SCOPE}" "${COMPONENT}" "${BREAKING}" "${SKIP_CI}" "${MESSAGE}" "${ISSUES_STRING}" "${CO_AUTHOR_STRING}")" "${@}"
+}
