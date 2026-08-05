6fd14e8dcf5d168032ff8549c1a3581f547f6d0b
Vincent Boutour <bob@vibioh.fr>
2026-08-05 11:35:29 +0200

    feat(sublime): Use dev version for smerge too
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- installations/sublime.sh
+++ installations/sublime.sh
@@ -10,21 +10,29 @@ clean() {
 }
 
 install() {
-  if package_exists "sublime-merge"; then
-    packages_install_desktop "sublime-merge"
-  fi
-
   local SUBLIME_TEXT_VERSION
-  SUBLIME_TEXT_VERSION="$(sublime_dev_version)"
+  SUBLIME_TEXT_VERSION="$(sublime_text_dev_version)"
 
   local SUBLIME_TEXT_STABLE_VERSION
-  SUBLIME_TEXT_STABLE_VERSION="$(sublime_stable_version)"
+  SUBLIME_TEXT_STABLE_VERSION="$(sublime_text_stable_version)"
 
   if [[ ${SUBLIME_TEXT_VERSION} < ${SUBLIME_TEXT_STABLE_VERSION} ]]; then
     SUBLIME_TEXT_VERSION="${SUBLIME_TEXT_STABLE_VERSION}"
   fi
 
-  sublime_install "${SUBLIME_TEXT_VERSION}"
+  sublime_text_install "${SUBLIME_TEXT_VERSION}"
+
+  local SUBLIME_MERGE_VERSION
+  SUBLIME_MERGE_VERSION="$(sublime_merge_dev_version)"
+
+  local SUBLIME_MERGE_STABLE_VERSION
+  SUBLIME_MERGE_STABLE_VERSION="$(sublime_merge_stable_version)"
+
+  if [[ ${SUBLIME_MERGE_VERSION} < ${SUBLIME_MERGE_STABLE_VERSION} ]]; then
+    SUBLIME_MERGE_VERSION="${SUBLIME_MERGE_STABLE_VERSION}"
+  fi
+
+  sublime_merge_install "${SUBLIME_MERGE_VERSION}"
 
   if command -v subl >/dev/null 2>&1; then
     "${DOTFILES_DIR}/tools/sublime/init.sh"
