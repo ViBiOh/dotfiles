9dd439d7f2f60fe4cbcf42e2213d65ae7822e3a1
Vincent Boutour <bob@vibioh.fr>
2026-08-14 18:21:12 +0200

    feat: Revert to cask install
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- installations/sublime.sh
+++ installations/sublime.sh
@@ -10,6 +10,12 @@ clean() {
 }
 
 install() {
+  if package_exists "sublime-merge@dev"; then
+    packages_install_desktop "sublime-merge@dev"
+  elif package_exists "sublime-merge"; then
+    packages_install_desktop "sublime-merge"
+  fi
+
   local SUBLIME_TEXT_VERSION
   SUBLIME_TEXT_VERSION="$(sublime_text_dev_version)"
 
@@ -21,18 +27,6 @@ install() {
   fi
 
   sublime_text_install "${SUBLIME_TEXT_VERSION}"
-
-  local SUBLIME_MERGE_VERSION
-  SUBLIME_MERGE_VERSION="$(sublime_merge_dev_version)"
-
-  local SUBLIME_MERGE_STABLE_VERSION
-  SUBLIME_MERGE_STABLE_VERSION="$(sublime_merge_stable_version)"
-
-  if [[ ${SUBLIME_MERGE_VERSION} < ${SUBLIME_MERGE_STABLE_VERSION} ]]; then
-    SUBLIME_MERGE_VERSION="${SUBLIME_MERGE_STABLE_VERSION}"
-  fi
-
-  sublime_merge_install "${SUBLIME_MERGE_VERSION}"
 
   if command -v subl >/dev/null 2>&1; then
     "${DOTFILES_DIR}/tools/sublime/init.sh"
